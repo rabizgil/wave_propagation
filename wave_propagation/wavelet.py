511d2ed736099422a1fcb5227221bc9b4acb602c
rabizgil <ruslan.abizgildin@gmail.com>
2025-05-08 20:37:49 +0100

    Add pytorch implementation

--- wave_propagation/wavelet.py
+++ wave_propagation/wavelet.py
@@ -1,10 +1,11 @@
 import math
 
 import numpy as np
+import torch
 
 
 # https://en.wikipedia.org/wiki/Gaussian_function
-def generate_gaussian_wavelet(npoints: int = 10, sigma: float = 2.4) -> np.ndarray:
+def generate_gaussian_wavelet(npoints: int = 10, sigma: float = 2.4, device="cpu") -> torch.Tensor:
     """
     Generates a Gaussian wavelet.
 
@@ -21,10 +22,10 @@ def generate_gaussian_wavelet(npoints: int = 10, sigma: float = 2.4) -> np.ndarr
         np.linspace(-npoints, npoints, 2 * npoints + 1),
     )
     wavelet = 300 / (sigma * 2 * math.pi) * (math.sqrt(2 * math.pi)) * np.exp(-0.5 * ((xx**2 + yy**2) / (sigma**2)))
-    return wavelet
+    return torch.from_numpy(wavelet).to(device)
 
 
-def generate_ricker_wavelet(npoints: int = 10, sigma: float = 2.4) -> np.ndarray:
+def generate_ricker_wavelet(npoints: int = 10, sigma: float = 2.4, device="cpu") -> torch.Tensor:
     """
     Generates a 2D Ricker (Mexican Hat) wavelet.
 
@@ -42,18 +43,20 @@ def generate_ricker_wavelet(npoints: int = 10, sigma: float = 2.4) -> np.ndarray
     )
     wavelet = ((xx) ** 2 + (yy) ** 2) / (2 * sigma**2)
     wavelet = 300 * (1 - wavelet) * np.exp(-wavelet)
-    return wavelet
+    return torch.from_numpy(wavelet).to(device)
 
 
-def inject_wavelet(wavefield: np.ndarray, wavelet: np.ndarray, x: int, y: int, scale: int | float = 1) -> np.ndarray:
+def inject_wavelet(
+    wavefield: torch.Tensor, wavelet: torch.Tensor, x: int, y: int, scale: int | float = 1
+) -> torch.Tensor:
     """
     Takes a wavefield array containing previous, current and future steps,
     wavelet array and injects the given wavelet into the wavefiled at position x, y.
 
     Args:
-        wavefield : np.ndarray
+        wavefield : torch.Tensor
             Wavefield of shape (3, size_x, size_y) containing previous, current and n+1 steps in size_x * size_y media.
-        wavelet : np.ndarray
+        wavelet : torch.Tensor
             Wavelet array to be injected.
         x : int
             Horizontal coordinate of the injection point.
